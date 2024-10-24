80724520880b07afb1ec7aee8fc58c2f123bb19a
Toni Menzel <toni.menzel@rebaze.com>
2024-10-24 14:38:33 +0200

    spawn instead of exec

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const core = require('@actions/core');
-var exec = require('child_process').exec;
+const { spawn } = require('node:child_process');
 
 async function run() {
     try {
-       core.info('Hey, opening OpenConnect Connection to ' + endpoint);
        const endpoint = core.getInput('endpoint');
        const user = core.getInput('user');
        const password = core.getInput('password');
+       core.info('Hey there, opening OpenConnect Connection to ' + endpoint + ' with user ' + user);
 
-       var openconnect_process = exec('echo "'+password+'" | openconnect  --user='+user+' -b --passwd-on-stdin ' + endpoint + ' --no-dtls', function(error, stdout, stderr) {
-            if (error) {
-                core.setFailed(error.message);
-                return;
-            }
-            core.info('OpenConnect connection opened');
+        // Define environment variables
+        const env = {
+            VPN_USER: user,
+            VPN_PASSWORD: password,
+            ENDPOINT: endpoint
+        };
+        
+        // Spawn the child process to run the openconnect command
+        const openconnect = spawn('bash', ['-c', `echo "${env.VPN_PASSWORD}" | openconnect --user=${env.VPN_USER} -b --passwd-on-stdin ${env.VPN_USER} --no-dtls`], {
+            env: process.env
         });
-
-        openconnect_process.stdout.on('data', function(data) {
-            console.log(data); 
+        // Handle stdout
+        openconnect.stdout.on('data', (data) => {
+            core.info(`stdout: ${data}`);
+        });
+        
+        // Handle stderr
+        openconnect.stderr.on('data', (data) => {
+            core.error(`stderr: ${data}`);
+        });
+        
+        // Handle when the process closes
+        openconnect.on('close', (code) => {
+            core.info(`openconnect process exited with code ${code}`);
         });
 
     } catch (error) {
